b3e6275cf41577d0a827dc9cf43850487af3ed26
Shovit Regmi <regmishovit12@gmail.com>
2026-08-12 22:26:39 +0545

    Stmp port 465

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
